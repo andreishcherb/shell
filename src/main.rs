a951563cc526c30a5513bfa42fc9c600a8a7585a
andreyscherbin <andrey123scherbin@gmail.com>
2025-11-09 21:47:10 +0300

    implement a REPL

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,16 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let mut command = String::new();
+        let mut command = String::new();
 
-    io::stdin()
-        .read_line(&mut command)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Failed to read line");
 
-    println!("{}: command not found", command.trim())
+        println!("{}: command not found", command.trim())
+    }
 }
